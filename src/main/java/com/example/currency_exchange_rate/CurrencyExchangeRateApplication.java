0f0e0c6b540118af048db3ea0a8d821ca6ee3426
Aleksey Pakhomov <Alexey007-89@mail.ru>
2022-04-11 10:51:56 +1000

    Initial commit

---
+++ src/main/java/com/example/currency_exchange_rate/CurrencyExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.currency_exchange_rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeRateApplication.class, args);
+    }
+
+}
